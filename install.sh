1a87d9f24023e315bcd0147057981c0064618508
Evan Keeton <keetonnj@edison.k12.nj.us>
2018-11-11 22:46:47 -0500

    Add basic install.sh, not yet in README

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+location="$1"
+
+if [ "$location" == "" ]; then
+  location='/usr/local/bin'
+fi
+
+if [ -f "$location/mycal" ]; then
+  echo foo
+  rm "$location/wordsearch"
+fi
+
+ln -s $(pwd)/python/master.py $location/wordsearch
